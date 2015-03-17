6ba124dac02c722967df5d1df7d7aafd01c65c75
fsuskate <francis.surroca@gmail.com>
2015-03-16 22:03:22 -0400

    Added images

--- ColorPad/ViewController.h
+++ ColorPad/ViewController.h
@@ -8,6 +8,23 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    CGPoint lastPoint;
+    CGFloat red;
+    CGFloat green;
+    CGFloat blue;
+    CGFloat brush;
+    CGFloat opacity;
+    
+    BOOL mouseSwiped;
+}
+
+@property (weak, nonatomic) IBOutlet UIImageView *mainImage;
+
+@property (weak, nonatomic) IBOutlet UIImageView *tempDrawImage;
+
+- (IBAction)pencilPressed:(id)sender;
+- (IBAction)reset:(id)sender;
+- (IBAction)save:(id)sender;
 
 @end
